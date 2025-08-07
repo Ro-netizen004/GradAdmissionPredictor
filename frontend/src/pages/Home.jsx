dc979ff714dff7010698ff192ea65632aeab038d
Rodela Ghosh <ghoshrodela111@gmail.com>
2025-08-07 02:07:45 -0400

    Updated fetch url for deployment

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -69,7 +69,7 @@ export default function Home() {
     };
 
     try {
-      const res = await fetch("http://127.0.0.1:5000/predict", {
+      const res = await fetch("https://gradadmissionpredictorbackend.onrender.com/predict", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(payload),
